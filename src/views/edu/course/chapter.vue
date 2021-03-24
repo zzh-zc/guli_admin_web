17483fb67ab87a7b3aeff3df472bd06b73fc95a9
zhuzh <1135902780@qq.com>
2021-03-24 17:27:23 +0800

    22

--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -74,7 +74,25 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="上传视频">
-          <!-- TODO -->
+          <el-upload
+            :on-success="handleVodUploadSuccess"
+            :on-remove="handleVodRemove"
+            :before-remove="beforeVodRemove"
+            :on-exceed="handleUploadExceed"
+            :file-list="fileList"
+            :action="BASE_API+'/vod/video/upload'"
+            :limit="1"
+            class="upload-demo">
+            <el-button size="small" type="primary">上传视频</el-button>
+            <el-tooltip placement="right-end">
+              <div slot="content">最大支持1G，<br>
+                支持3GP、ASF、AVI、DAT、DV、FLV、F4V、<br>
+                GIF、M2T、M4V、MJ2、MJPEG、MKV、MOV、MP4、<br>
+                MPE、MPG、MPEG、MTS、OGG、QT、RM、RMVB、<br>
+                SWF、TS、VOB、WMV、WEBM 等视频格式上传</div>
+              <i class="el-icon-question"/>
+            </el-tooltip>
+          </el-upload>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -88,6 +106,7 @@
 <script>
 import chapter from '@/api/chapter/chapter'
 import video from '@/api/video/video'
+import vod from '@/api/vod/vod'
 export default {
   data() {
     return {
@@ -106,8 +125,11 @@ export default {
         title: '',
         sort: 0,
         free: 0,
-        videoSourceId: ''
+        videoSourceId: '',
+        videoOriginalName:''
       },
+      fileList: [],//上传文件列表
+      BASE_API: process.env.BASE_API // 接口API地址
     }
   },
 
@@ -120,6 +142,30 @@ export default {
   },
 
   methods: {
+    //成功回调
+    handleVodUploadSuccess(response, file, fileList) {
+      this.video.videoSourceId = response.data.videoId;
+      this.video.videoOriginalName = file.name;
+    },
+    //视图上传多于一个视频
+    handleUploadExceed(files, fileList) {
+      this.$message.warning('想要重新上传视频，请先删除已上传的视频')
+    },
+    beforeVodRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`)
+    },
+    handleVodRemove(file, fileList) {
+      console.log(file)
+      vod.removeById(this.video.videoSourceId).then(response=>{
+        this.video.videoSourceId = ''
+        this.video.videoOriginalName = ''
+        this.fileList = []
+        this.$message({
+          type: 'success',
+          message: response.message
+        })
+      })
+    },
     saveOrUpdateVideo() {
       this.saveVideoBtnDisabled = true
       if (!this.video.id) {
@@ -154,6 +200,7 @@ export default {
       this.dialogVideoFormVisible = true
       video.getVideoInfoById(videoId).then(response => {
         this.video = response.data.item
+        this.fileList = [{'name': this.video.videoOriginalName}]
       })
     },
     removeVideo(videoId) {
